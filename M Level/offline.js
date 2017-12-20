57c5b39330916fd362719b85ba10376469cf7019
Ben Seeley <32374033+SeeBen@users.noreply.github.com>
2017-12-20 09:46:31 -0800

    Add files via upload

---
+++ M Level/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1513791904,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/topspawner-sheet0.png",
+		"images/matsukawa-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
